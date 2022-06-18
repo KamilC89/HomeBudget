ed8476163290db357cf7df233db1966fe8c4c03a
Kamilc <kamilcuber@gmail.com>
2022-06-19 00:31:33 +0200

    Initial commit

---
+++ src/test/java/com/mybudget/HomeBudget/HomeBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybudget.HomeBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
